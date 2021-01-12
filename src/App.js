5f59e170535496446add7e667ecdce2311cb14f2
Matias Ovejero <61472081+MatiasOvejero@users.noreply.github.com>
2021-01-12 16:03:58 -0300

    Returns info even if one input is missing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import "./styles/App.css";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
-import "./styles/App.css";
 
 function App() {
   const [weather, setWeather] = useState([]);
   const APIKEY = "08556e7069d24a375921151a0e39f8c1";
 
   async function fetchData(e) {
-    console.log(e);
-    e.target.preventDefault();
-    const apiData = await fetchData(
-      `api.openweathermap.org/data/2.5/weather?q=London&appid=${APIKEY}`
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.city.value;
+    e.preventDefault();
+    const apiData = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}}&APPID=${APIKEY}`
     )
       .then((res) => res.json())
       .then((data) => data);
-    setWeather({
-      data: apiData,
-    });
+    if (city && country) {
+      setWeather({
+        data: apiData,
+        city: apiData.city,
+        country: apiData.sys.country,
+        description: apiData.weather[0].description,
+        temperature: apiData.main.temp,
+        error: "",
+      });
+    } else {
+      setWeather({
+        data: " ",
+        city: " ",
+        country: " ",
+        description: " ",
+        temperature: " ",
+        error: "Escribe el pais o la ciudad faltante por favor",
+      });
+    }
   }
+
   return (
     <div className="App">
-      <h3>Weather App</h3>
+      <h3>WEATHER APP</h3>
       <Form getWeather={fetchData} />
+      <Weather
+        city={weather.city}
+        country={weather.country}
+        description={weather.description}
+        temperature={weather.temperature}
+        error={weather.error}
+      />
       {console.log(weather.data)}
     </div>
   );
@@ -30,4 +55,4 @@ function App() {
 
 export default App;
 
-//api.openweathermap.org/data/2.5/weather?q=London,uk&appid={APIKEY}
+//api.openweathermap.org/data/2.5/weather?q=London,uk&appid={APIKEY}08556e7069d24a375921151a0e39f8c1
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Form = ({ getWeather }) => {
+const Form = (props) => {
   return (
-    <form onSubmit={getWeather}>
+    <form onSubmit={props.getWeather}>
       <input type="text" placeholder="city" name="city" />
       <input type="text" placeholder="country" name="country" />
       <button>Submit</button>
     </form>
   );
 };
-
 export default Form;

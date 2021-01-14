5c31203f35038f20e8111d9a42e98e30b9536b35
MatiasOvejero <matilmds91@gmail.com>
2021-01-14 16:12:56 -0300

    Until clear sky

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,14 +5,26 @@ import React from "react";
 const Weather = ({ description, city, country, error, temperature }) => {
   function matchValues () {
     if(description) {
-        const weatherDescription = description.split(' ')
+        const weatherDescription = description.split(' ').join('')
         const keyWords = ['cloudy','clouds', 'cloud', 'overcast']
+        const keyWordsSun = ['clearsky']
+        const keyWordsRain = ['rain', 'moderate rain']
+        const keyWordsSnow = ['snow', 'moderate snow']
         for(let i = 0; i < weatherDescription.length; i++) {
             if(keyWords.includes(weatherDescription[i])) {
-                return <img src='https://media.freestocktextures.com/cache/74/8b/748ba3fe5976d8b03219a64851d2790d.jpg' />
-            }
-        }
-    }
+                return <img src='https://www.quotemaster.org/images/5a/5aed04eac38dd9ad0450cfd9403b2654.jpg' />
+            } else if (keyWordsSun.includes(weatherDescription.split(' ').join(''))) {
+                return <img src='https://www.quotemaster.org/images/5a/5aed04eac38dd9ad0450cfd9403b2654.jpg'/>
+              }
+              else if (keyWordsRain.includes(weatherDescription[i])) {
+                return <img src='https://pennalumniblog.files.wordpress.com/2012/01/rainy_day.jpg'/>
+              }
+              else if (keyWordsSnow.includes(weatherDescription[i])) {
+                return <img src='https://thumbor-prd.zoover.cloud/unsafe/trim/600x/images/testimonials/original/00610000-00619999/T617250.jpg'/>
+              };
+              console.log(weatherDescription);
+        }      
+      }
   }
   return (
     <div>
